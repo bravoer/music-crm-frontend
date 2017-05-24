3e4459e2056ee9204ed0104c91a8af6120a29f8e
Erika Pauwels <erika.pauwels@gmail.com>
2017-05-24 22:50:03 +0200

    Upgrade to Ember v2.12.2

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,5 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+/* eslint-env node */
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var nodeSass = require('node-sass'); // loads the version in your package.json
 
 module.exports = function(defaults) {
